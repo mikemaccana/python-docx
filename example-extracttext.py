650d586779391b489596df6024f413bd754ae6a6
Steve Canny <scanny@cisco.com>
2013-07-16 22:32:20 -0700

    PEP8 example-extracttext.py

--- example-extracttext.py
+++ example-extracttext.py
@@ -1,30 +1,37 @@
-#!/usr/bin/env python2.6
-'''
+#!/usr/bin/env python
+"""
 This file opens a docx (Office 2007) file and dumps the text.
 
-If you need to extract text from documents, use this file as a basis for your work.
+If you need to extract text from documents, use this file as a basis for your
+work.
 
 Part of Python's docx module - http://github.com/mikemaccana/python-docx
 See LICENSE for licensing information.
-'''
-from docx import *
+"""
+
 import sys
-if __name__ == '__main__':        
+
+from docx import opendocx, getdocumenttext
+
+if __name__ == '__main__':
     try:
         document = opendocx(sys.argv[1])
-        newfile = open(sys.argv[2],'w')        
+        newfile = open(sys.argv[2], 'w')
     except:
-        print('Please supply an input and output file. For example:')    
-        print('''  example-extracttext.py 'My Office 2007 document.docx' 'outputfile.txt' ''')    
+        print(
+            "Please supply an input and output file. For example:\n"
+            "  example-extracttext.py 'My Office 2007 document.docx' 'outp"
+            "utfile.txt'"
+        )
         exit()
-    ## Fetch all the text out of the document we just created        
-    paratextlist = getdocumenttext(document)    
 
-    # Make explicit unicode version    
+    # Fetch all the text out of the document we just created
+    paratextlist = getdocumenttext(document)
+
+    # Make explicit unicode version
     newparatextlist = []
     for paratext in paratextlist:
-        newparatextlist.append(paratext.encode("utf-8"))                  
-    
-    ## Print our documnts test with two newlines under each paragraph
+        newparatextlist.append(paratext.encode("utf-8"))
+
+    # Print out text of document with two newlines under each paragraph
     newfile.write('\n\n'.join(newparatextlist))
-    #print '\n\n'.join(newparatextlist)

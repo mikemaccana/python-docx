37a3709b451528b1a14976ab12958d9103b8f86f
Steve Canny <scanny@cisco.com>
2014-01-13 16:48:38 -0800

    release: v0.2.3 -- change PIL dependency to Pillow

--- setup.py
+++ setup.py
@@ -11,20 +11,26 @@ from distutils.command.install import INSTALL_SCHEMES
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
 
-setup(name='docx',
-      version='0.2.2',
-      install_requires=['lxml', 'PIL'],
-      description='The docx module creates, reads and writes Microsoft Office Word 2007 docx files',
-      author='Mike MacCana',
-      author_email='python-docx@googlegroups.com',
-      maintainer='Steve Canny',
-      maintainer_email='python-docx@googlegroups.com',
-      url='http://github.com/mikemaccana/python-docx',
-      py_modules=['docx'],
-      data_files=[
-          ('docx-template/_rels', glob('template/_rels/.*')),
-          ('docx-template/docProps', glob('template/docProps/*.*')),
-          ('docx-template/word', glob('template/word/*.xml')),
-          ('docx-template/word/theme', glob('template/word/theme/*.*')),
-          ],
-      )
+DESCRIPTION = (
+    'The docx module creates, reads and writes Microsoft Office Word 2007 do'
+    'cx files'
+)
+
+setup(
+    name='docx',
+    version='0.2.3',
+    install_requires=['lxml', 'Pillow>=2.0'],
+    description=DESCRIPTION,
+    author='Mike MacCana',
+    author_email='python-docx@googlegroups.com',
+    maintainer='Steve Canny',
+    maintainer_email='python-docx@googlegroups.com',
+    url='http://github.com/mikemaccana/python-docx',
+    py_modules=['docx'],
+    data_files=[
+        ('docx-template/_rels',      glob('template/_rels/.*')),
+        ('docx-template/docProps',   glob('template/docProps/*.*')),
+        ('docx-template/word',       glob('template/word/*.xml')),
+        ('docx-template/word/theme', glob('template/word/theme/*.*')),
+    ],
+)
